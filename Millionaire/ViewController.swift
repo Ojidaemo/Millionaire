e4db8bd51af2caaaeba29939c524c8ec92273d6f
Ojidaemo <vit.martsinovich@gmail.com>
2023-02-06 12:18:39 +0300

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Vitali Martsinovich on 2023-02-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
