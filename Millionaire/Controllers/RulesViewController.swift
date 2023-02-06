9cad66c093d4e88d3b2c46650fe06d020f2f17b0
Ojidaemo <vit.martsinovich@gmail.com>
2023-02-06 12:51:50 +0300

    Add rules screen and vc

---
+++ Millionaire/Controllers/RulesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RulesViewController.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Vitali Martsinovich on 2023-02-05.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func mainMenuPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true)
+        
+    }
+    
+}
